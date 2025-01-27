95925d6f2026767a1c0dda1e4fe5b8e5f6cc0c93
i-ankit-here <closetooankit@gmail.com>
2025-01-28 01:11:04 +0530

    review Controllers

---
+++ src/api/controllers/reviewControllers.js
@@ -0,0 +1,105 @@
+import { Review } from "../../models/Review.js"
+
+export const createReview = async (req, res, next) => {
+  try {
+    const { pickupId, userId, vendorId, rating, comment } = req.body;
+
+    if([pickupId,userId,vendorId,rating,comment].some((item)=>{
+        return !item;
+    })){
+        res.status(401);
+        throw new Error("All details are necessary")
+    }
+
+    const review = await Review.create({
+        pickup : pickupId,
+        user : userId,
+        vendor : vendorId,
+        rating : rating,
+        comment : comment
+    })
+
+    res.status(201).json(review)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const updateReview = async (req, res, next) => {
+  try {
+    const { reviewId ,pickupId, userId, vendorId, rating, comment } = req.body;
+    if(!reviewId && !pickupId){
+        res.status(401);
+        throw new Error("Please provide ReviewId or pickupId")
+    }
+    let review = {}
+    if(reviewId){
+        review = await Review.findById(reviewId);
+    }
+    else {
+        review = await Review.findOne({
+        pickup : pickupId
+        })
+    }
+    if(!review){
+        res.status(401);
+        throw new Error("Wrong reviewId provided")
+    }
+
+    review.pickup = pickupId?pickupId : review.pickup;
+    review.user = userId?userId : review.user;
+    review.vendor = vendorId?vendorId : review.vendor;
+    review.rating = rating?rating: review.rating;
+    review.comment = comment?comment : review.comment;
+    
+    await review.save();
+    const updatedReview = await Review.findById(review._id);
+
+    res.status(201).json(updatedReview);
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getAllReviewsByUser = async (req, res, next) => {
+  try {
+    const userId = req.user?._id;
+    if(!userId){
+        res.status(401);
+        throw new Error("Unauthorized");
+    }
+    let reviews = [];
+    if(req.isVendor){
+        reviews = await Review.find({
+            vendor : userId
+        })
+    }else{
+        reviews = await Review.find({
+            user : userId
+        })
+    }
+    
+    res.status(201).json({"data":reviews});
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getReviewByPickup = async (req, res, next) => {
+    try {
+      const {pickupId} = req.params;
+      if(!pickupId){
+          res.status(401);
+          throw new Error("Provide pickup Id");
+      }
+
+      const review = await Review.findOne({pickup:pickupId})
+      
+      res.status(201).json(review);
+  
+    } catch (error) {
+      next(error)
+    }
+  }
