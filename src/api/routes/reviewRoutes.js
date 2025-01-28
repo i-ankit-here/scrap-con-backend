79614349e0b315d690ddd65ddfd52843de2d419f
i-ankit-here <closetooankit@gmail.com>
2025-01-29 01:27:41 +0530

    changed route namings for conventions

--- src/api/routes/reviewRoutes.js
+++ src/api/routes/reviewRoutes.js
@@ -11,9 +11,9 @@ import{
 const router = express.Router()
 
 router.post("/createReview",protect, createReview)
-router.post("/updateReview",protect, updateReview)
+router.put("/updateReview",protect, updateReview)
 router.get("/getAllReviewsByUser", protect, getAllReviewsByUser)
 router.get("/getReviewByPickup/:pickupId", protect, getReviewByPickup)
-router.get("/deleteReview/:reviewId", protect, deleteReview)
+router.delete("/deleteReview/:reviewId", protect, deleteReview)
 
 export default router
